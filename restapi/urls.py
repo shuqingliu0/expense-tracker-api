03756aa87d5ba45a782b556c4c7d2c910209cb67
shuqingliu0 <shuqing.liu@gmail.com>
2023-06-04 18:18:17 -0700

    Added filtering support to listing view.

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from restapi import views
 
 urlpatterns = [
-    path("expense/", views.ExpenseListCreate.as_view(), name="expense-list-create"),
+    path("expenses", views.ExpenseListCreate.as_view(), name="expense-list-create"),
     path(
-        "expense/<pk>",
+        "expenses/<pk>",
         views.ExpenseRetrieveDelete.as_view(),
         name="expense-retrieve-delete",
     ),
